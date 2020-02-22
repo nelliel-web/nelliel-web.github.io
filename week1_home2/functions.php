ff4f082c51cde3662357ad76834eaec98594800d
Alexandr Kuznetsov <nelliel.web@gmail.com>
2020-02-22 11:00:53 +0200

    1 week. complete hw 2-1

--- week1_home2/functions.php
+++ week1_home2/functions.php
@@ -1,10 +1,43 @@
 <?php
-function task1() {
-    echo 'task1';
+
+
+function task1($array, $optional = false)
+{
+    /*
+    Функция должна принимать массив строк и выводить каждую строку в отдельном параграфе (тег <p>)
+    Если в функцию передан второй параметр true, то возвращать (через return) результат в виде одной объединенной строки.
+    */
+    if (is_array($array)) {
+        if ($optional === false) {
+            foreach ($array as $text) {
+                if (is_string($text)) {
+                    echo '<p>' . $text . '</p>';
+                }
+            }
+        } elseif ($optional === true) {
+            $result = '';
+            foreach ($array as $text) {
+                if (is_string($text)) {
+                    $result .= ' ' . $text;
+                }
+            }
+            return $result;
+        } else {
+            return false;
+            //Второй парамент функции не является boolean
+        }
+    }
+    return false;
+    //Первый параментр функции не является массивом
 }
-function task2() {
+
+
+function task2()
+{
     echo 'task2';
 }
-function task3() {
+
+function task3()
+{
     echo 'task3';
 }
--- week1_home2/index.php
+++ week1_home2/index.php
@@ -1,5 +1,9 @@
 <?php
 require('functions.php');
-task1();
+
+$forTask1 = ['Lorem ipsum dolor sit amet', 'consectetur adipisicing elit', 'sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua', ['text','text2'], 123, '911'];
+task1($forTask1, false);
+
 task2();
 task3();
